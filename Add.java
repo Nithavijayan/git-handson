b3244e85289dc3c13a350ff45843b53ae89ba14b
Nithavijayan <nitha.v.avrn@gmail.com>
2024-11-13 16:55:11 +0530

    adding java program file

---
+++ Add.java
@@ -0,0 +1,14 @@
+public class Add
+{
+ public static void main(String [] args)
+ {
+    float num1,sum;
+	int num2;
+	num1=961.25f;
+	num2=12;
+	sum=num1+num2;
+	
+	System.out.println("sum= "+sum);
+ }
+
+}
